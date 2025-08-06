0e6c132ef650e437187582d717f1bfb637900640
shazaniyu55 <shazaniyu@gmail.com>
2025-08-06 09:43:50 -0700

    'new'

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import NavBar from './components/navbar.jsx'
+import Hero from './components/hero.jsx'
+import Community from './components/card1.jsx'
+import Hero3 from './components/hero3.jsx'
+import Hero4 from './components/hero4.jsx'
+import Hero5 from './components/hero5.jsx'
+import Hero6 from './components/hero6.jsx'
+
+const App = ()=> {
+
+  return (
+    <>
+      <NavBar/>
+      <Hero/>
+      <Community/>
+      <Hero3/>
+      <Hero4/>
+      {/* <Hero5/> */}
+      <Hero6/>
+      
+
+      
+    </>
+  )
+}
+
+export default App
