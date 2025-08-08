d5fe52276335a85b15700f14e4805d2bb7497c75
shazaniyu55 <shazaniyu@gmail.com>
2025-08-08 06:32:27 -0700

    'new1'

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -90,7 +90,7 @@ const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
                 </div>
                 <div className="py-6">
                   <a
-                    href="#"
+                    href="/login"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                   >
                     Log in
