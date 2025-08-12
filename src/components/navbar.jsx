8d3a9df62ee53038dc382750fe070e1184bf3790
shazaniyu55 <shazaniyu@gmail.com>
2025-08-12 08:20:51 -0700

    'new8'

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react';
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-
+import { motion } from "framer-motion";
 const navigation = [
   { name: 'Home', href: '/' },
   { name: 'About', href: '/about' },
@@ -143,21 +143,35 @@ function NavBar() {
       {/* HERO CONTENT */}
       <div className="relative z-10 px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 text-center">
-          <h1 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl drop-shadow-lg">
-            The Royale Cleaners Home and Office Cleaning Services in UK
-          </h1>
-          <p className="mt-8 text-lg font-medium text-white sm:text-xl/8 drop-shadow-lg">
-            Book a verified cleaning person for your home or office in UK.
-          </p>
-          {/* <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="#"
-              className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-yellow-500"
-            >
-              Book Now
-            </a>
-            
-          </div> */}
+         <motion.h1
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, ease: "easeOut" }}
+        className="text-5xl font-semibold tracking-tight text-white sm:text-7xl drop-shadow-lg"
+      >
+        The Royale Cleaners Home and Office Cleaning Services in UK
+      </motion.h1>
+           <motion.p
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.3, duration: 1, ease: "easeOut" }}
+        className="mt-8 text-lg font-medium text-white sm:text-xl/8 drop-shadow-lg"
+      >
+        Book a verified cleaning person for your home or office in UK.
+      </motion.p>
+           <motion.div
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ delay: 0.6, duration: 0.8, ease: "easeOut" }}
+        className="mt-10 flex items-center justify-center gap-x-6"
+      >
+        <a
+          href="#"
+          className="rounded-md sm:w-[300px] h-[40px] bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-yellow-500"
+        >
+          Book Now
+        </a>
+      </motion.div>
         </div>
       </div>
 
